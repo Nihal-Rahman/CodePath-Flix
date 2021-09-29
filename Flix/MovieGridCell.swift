d15bc3319bc3c2b8f816ffecdcfa6ae462c6e3be
Nihal-Rahman <83567449+Nihal-Rahman@users.noreply.github.com>
2021-09-28 21:45:39 -0400

    Assignment 2 flix
    Assignment 2 completed flix

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Nihal Rahman on 9/28/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
